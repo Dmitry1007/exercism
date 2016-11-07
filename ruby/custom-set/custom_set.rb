066356c7557fdeac5d0caf5aad863335b37c1897
Dmitry Sky <dmitry.sky@icloud.com>
2016-11-07 08:56:41 -0700

    start ruby custom set exercism from scratch

--- ruby/custom-set/custom_set.rb
+++ ruby/custom-set/custom_set.rb
@@ -6,31 +6,6 @@ class CustomSet
   end
 
   def empty?
-    !@data.any?
-  end
-
-  def member?(element)
-    @data.include?(element)
-  end
-
-  def subset?(set)
-    @data.all? do |element|
-      set.member?(element)
-    end
-  end
-
-  def disjoint?(set)
-    @data.none? do |element|
-      set.member?(element)
-    end
-  end
-
-  def ==(set)
-    set.data.sort == data.sort
-  end
-
-  def add(element)
-    data.push(element)
-    self
+    data.count == 0 ? true : false
   end
 end
